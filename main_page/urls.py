fe507277bdc2150abab135326ab678cf8c1bcae2
lepri4dw <lepri4dw@gmail.com>
2023-12-04 13:44:54 +0600

    Made 'dishes' and 'about us' pages, added search for dishes

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from main_page.views import dishes_list_view, dish_list_detail_view
+from main_page.views import main_list_view, dishes_list_view, dish_list_detail_view, SearchView, about_us_view
 
 urlpatterns = [
-    path('', dishes_list_view),
+    path('', main_list_view, name='home'),
+    path('about/', about_us_view, name='about'),
+    path('dishes/', dishes_list_view, name='dishes'),
     path('dish_list/<int:id>/', dish_list_detail_view),
+    path('search/', SearchView.as_view(), name='search')
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
 from .models import DishListModel, Orders, Slider
 
-def dishes_list_view(request):
+
+def main_list_view(request):
     if request.method == "GET":
         dishes_list = DishListModel.objects.all()
         orders_list = Orders.objects.all()
@@ -11,7 +14,35 @@ def dishes_list_view(request):
             'orders_list': orders_list,
             'slider_list': slider_list
         })
+
+
+def dishes_list_view(request):
+    if request.method == 'GET':
+        dishes_list = DishListModel.objects.all()
+        return render(request, template_name='dishes/dishes.html', context={
+            'dishes_list': dishes_list,
+        })
+
+
 def dish_list_detail_view(request, id):
     if request.method == 'GET':
         dish_id = get_object_or_404(DishListModel, id=id)
         return render(request, template_name='dish_detail.html', context={'dish': dish_id})
+
+
+class SearchView(generic.ListView):
+    template_name = 'dishes/dishes.html'
+    context_object_name = 'dishes_list'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return DishListModel.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('id')
+        return context
+
+
+def about_us_view(request):
+    return render(request, template_name='about.html')
